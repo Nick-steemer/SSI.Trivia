d3e171141f129bfb0c507ab5850ac9b2a642f95c
Nick McClurg <nmcclurg@steemer.com>
2025-04-09 06:33:57 -0400

    updated packages and cleaned up dbcontext

--- SSI.Trivia/SSI.Trivia.Shared/DbContexts/TriviaDbContext.cs
+++ SSI.Trivia/SSI.Trivia.Shared/DbContexts/TriviaDbContext.cs
@@ -3,14 +3,9 @@ using SSI.Trivia.Shared.Models;
 
 namespace SSI.Trivia.Shared.DbContexts;
 
-public class TriviaDbContext : DbContext
+public class TriviaDbContext(DbContextOptions<TriviaDbContext> options) : DbContext(options)
 {
     public DbSet<Game> Games { get; set; }
     public DbSet<Question> Questions { get; set; }
     public DbSet<Answer> Answers { get; set; }
-
-    public TriviaDbContext(DbContextOptions<TriviaDbContext> options)
-        : base(options)
-    {
-    }
 }
