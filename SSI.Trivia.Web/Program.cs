a0579a22fd8c28e30b15d1b2a987a3ab0cba2284
An-EpicBeard <anepicbeard@gmail.com>
2025-04-14 19:35:03 -0400

    adding active directory to participant (will need to be set up in octopus or need to ask ryan for the things) better visibility on scores vs review

--- SSI.Trivia.Web/Program.cs
+++ SSI.Trivia.Web/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
 using MudBlazor.Services;
 using SSI.Trivia.Shared.DbContexts;
 using SSI.Trivia.Shared.Hubs;
+using SSI.Trivia.Shared.Services;
 using SSI.Trivia.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Microsoft Identity authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddDbContext<TriviaDbContext>(options =>
 {
@@ -17,10 +31,6 @@ builder.Services.AddDbContext<TriviaDbContext>(options =>
 });
 builder.Services.AddSignalR();
 builder.Services.AddMudServices();
-
-#if DEBUG
-builder.Services.AddScoped(_ => new { IsDebugMode = true });
-#endif
 
 var app = builder.Build();
 
@@ -34,6 +44,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
